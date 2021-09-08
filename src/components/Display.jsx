1ebd8e9b4aeaeeb3771cc4c11eb0697c3c37a9db
Brianna Bias <brianna.bias@gmail.com>
2021-09-07 17:08:14 -0700

    color showing

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,3 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const Display = ({ color }) => {
+  return <div style={{ background: color }}>COLOR IS HERE</div>;
+};
+
+Display.propTypes = {
+  color: PropTypes.string.isRequired,
+};
+
+export default Display;
